b43cf283cb111186277ceea7d4aedd3015e131f5
Maxim Zaytsev <zaytsev.max90@gmail.com>
2015-09-10 17:03:11 +0300

    Быстрая правка

--- DB_SQL_Script/Patch for shop items properties values.sql
+++
@@ -1,3 +0,0 @@
-DELETE FROM `ShopItemsPropertiesValues` WHERE `value` = '';
-DELETE FROM `ShopItemsPropertiesValues` WHERE `value` IS NULL;
-UPDATE `ShopItemsPropertiesValues` SET `value` = REPLACE(`value`, ',', '.') WHERE `value` LIKE '%,%';
