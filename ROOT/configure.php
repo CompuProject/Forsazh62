27fe09b3baf8da4c692eba6e00673d2f40b989a7
Maxim Zaytsev <zaytsev.max90@gmail.com>
2015-09-23 13:27:25 +0300

    Исправление

--- ROOT/configure.php
+++ ROOT/configure.php
@@ -53,7 +53,7 @@
 global $_DBSETTINGS;
 $_DBSETTINGS['host'] = "localhost";
 $_DBSETTINGS['user'] = "root";
-$_DBSETTINGS['password'] = "Kt22Mx3y90";
+$_DBSETTINGS['password'] = "";
 $_DBSETTINGS['db_name'] = "forsazh62_db";
 $_DBSETTINGS['charset'] = "utf8";
 mb_internal_encoding("UTF-8");
